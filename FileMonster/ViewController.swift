f93ca6bceebc7171497ffb668257928ac46d213b
Slava Plis <observleer@gmail.com>
2018-07-27 15:08:05 +0300

    Initial Commit

---
+++ FileMonster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileMonster
+//
+//  Created by Slava Plis on 7/27/18.
+//  Copyright © 2018 Slava Plis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
