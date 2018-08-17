8632e6d0a9866f91f601dcc79fc3c5c230d6a3fd
Slava Plis <observleer@gmail.com>
2018-08-17 14:38:29 +0300

    Updated Operations table.

--- FileMonster/App/OperationsController.swift
+++ FileMonster/App/OperationsController.swift
@@ -10,9 +10,29 @@ import Cocoa
 
 class OperationsController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
 }
+
+extension OperationsController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 3
+    }
+}
+
+extension  OperationsController: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "OperationCell"), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = "OperationCell"
+            return cell
+        }
+        return nil
+    }
+}
+
+
