fce56b58cbe1ca1535bd4429bc81af249f50d6a6
Slava Plis <observleer@gmail.com>
2018-08-21 17:49:32 +0300

    Adding extensions.

--- FileMonster/App/ItemsController.swift
+++ FileMonster/App/ItemsController.swift
@@ -8,56 +8,29 @@
 
 import Cocoa
 
-class ItemsController: NSViewController, NSTableViewDataSource, FileLoaderDelegate {
+class ItemsController: NSViewController, NSTableViewDataSource {
     @IBOutlet weak var tableView: NSTableView!
     @IBOutlet weak var selectButton: NSButton!
     
-    private var fileLoader: Loader? {
-        didSet {
-            fileLoader?.delegate = self
-        }
-    }
+    private var fileLoader: Loader?
+    private let store = DataStore.shared
+
     var content: [File] = []
 
     // Service Dependency
     func set(loadService: Loader) {
         self.fileLoader = loadService
-        
     }
     
     // User Dialog
     @IBAction func selectFolder(_ sender: Any) {
-        let dialog = NSOpenPanel()
-        dialog.title = "Select Path"
-        dialog.message = "Please, select the Folder which shoud be processed by matched operations above."
-        dialog.allowsMultipleSelection = true
-        dialog.canChooseDirectories = true
-        
-        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
-            if let selectedPath = dialog.url {
-                guard let loader = fileLoader else { return }
-                self.content = loader.load(at: selectedPath)
-                print(content.count)
-                tableView.reloadData()
-
-            }
-        } else {
-            return
-        }
+        showDialog()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    // MARK: -
-    func didLoad(data: [File]) {
-        content = data
-        print(content.count)
-        tableView.reloadData()
-        print(content.count)
-    }
-    
     func numberOfRows(in tableView: NSTableView) -> Int {
         print("tbable: \(content.count)")
 
@@ -76,6 +49,25 @@ class ItemsController: NSViewController, NSTableViewDataSource, FileLoaderDelega
         }
         
         return "Check identifier for Cell ;P"
+    }
+}
+
+// MARK: - File Open Panel
+
+extension ItemsController {
+    func showDialog() {
+        let dialog = NSOpenPanel()
+        dialog.title = "Select Path"
+        dialog.message = "Please, select the Folder which shoud be processed by matched operations above."
+        dialog.allowsMultipleSelection = true
+        dialog.canChooseDirectories = true
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            if let selectedPath = dialog.url {
+                guard let loader = fileLoader else { return }
+                loader.load(path: selectedPath)
+            }
+        }
     }
 }
 
