d22f2f44cd37b9f43bee3bc935f21801537d0ebf
Slava Plis <observleer@gmail.com>
2018-08-29 20:09:23 +0300

    Refine structure, connected observer.

--- FileMonster/Service/OperationStack.swift
+++ FileMonster/Service/OperationStack.swift
@@ -8,51 +8,87 @@
 
 import Foundation
 
-protocol OperationStackDelegate {
-    func didUpdate()
-}
-
 class OperationStack {
+    
+    let concurentOperations: Int
+    
+    open static var `default`: OperationStack {
+        return OperationStack(concurentOperations: 5)
+    }
+    
     var stack: OperationQueue {
         let queue = OperationQueue()
-        queue.name = "Operation_Stack_Queue"
-        queue.maxConcurrentOperationCount = 4
+        queue.name = "com.mobndev.filemonster.queue"
+        queue.maxConcurrentOperationCount = 5
+        print("get com.mobndev.filemonster.queue")
         return queue
     }
     
-    /// Adding operation into Queue and Launch
+    init(concurentOperations: Int) {
+        self.concurentOperations = concurentOperations
+    }
+    
     func add(op: FileOperation) {
         stack.addOperation(op)
     }
+}
+
+protocol OperationProgressDelegate: AnyObject {
+    func didUpdateProgress(fractionCompleted: Double)
+}
+
+protocol OperationResultDelegate: AnyObject {
+    func someResult()
+}
+
+//protocol FileOperation: class {
+//    var type: OperationType { get }
+//    weak var delegate: OperationProgressDelegate? { get set }
+//}
+
+class FileOperation: Operation {
+    public weak var delegate: OperationProgressDelegate?
+    public var type: OperationType
+    internal var content: [File]
     
-    func run() {
-        
+    init(with content: [File], type: OperationType) {
+        self.type = type
+        self.content = content
     }
 }
 
-class FileOperation: Operation {
-//    var progress: Progress?
-    var inputContent: [File]
-    var result: [File] = []
-    
+class SearchingDuplicatesOperation: FileOperation {
+    private var kvo: NSKeyValueObservation?
+    private var manager = FileManager()
+    private var progress: Progress
+    private var result: [File] = []
+
     init(with content: [File]) {
-        self.inputContent = content
+        self.progress = Progress(totalUnitCount: Int64(content.count))
+        super.init(with: content, type: .duplicates)
     }
     
     override func main() {
-        let manager = FileManager()
-        for file in inputContent {
-            let res = inputContent.filter {
+        setProgressObserving()
+        
+        for file in content {
+            if progress.isCancelled { break }
+            
+            let res = content.filter {
                manager.contentsEqual(atPath: file.path.path, andPath: $0.path.path)
             }
-            print("\(inputContent.index(of: file)!) from \(String(describing:inputContent.count))")
-            
             if res.count > 1 { result.append(contentsOf: res) }
+            if let idx = content.index(of: file) {
+                progress.completedUnitCount = Int64(idx + 1)
+            }
         }
-        result.map { print($0.name)}
     }
     
+    // MARK: -  KVO
+    private func setProgressObserving() {
+        kvo = progress.observe(\.fractionCompleted) { [unowned self] (progress, change) in
+            print(progress.fractionCompleted)
+            self.delegate?.didUpdateProgress(fractionCompleted: progress.fractionCompleted)
+        }
+    }
 }
-
-
-
