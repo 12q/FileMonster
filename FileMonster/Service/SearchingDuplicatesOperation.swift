a76e146104e80e6dfd7e8939dd1b420d18e86a55
Slava Plis <observleer@gmail.com>
2018-08-30 20:12:33 +0300

    Moved into separate file.

--- FileMonster/Service/SearchingDuplicatesOperation.swift
+++ FileMonster/Service/SearchingDuplicatesOperation.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+class SearchingDuplicatesOperation: FileOperation {
+    private var kvo: NSKeyValueObservation?
+    private var manager = FileManager()
+    private var progress: Progress
+    private var result: [File] = []
+    
+    init(with content: [File]) {
+        self.progress = Progress(totalUnitCount: Int64(content.count))
+        super.init(with: content, type: .duplicates)
+    }
+    
+    override func main() {
+        setProgressObserving()
+        
+        for file in content {
+            if progress.isCancelled { break }
+            
+            let res = content.filter {
+                manager.contentsEqual(atPath: file.path.path, andPath: $0.path.path)
+            }
+            if res.count > 1 { result.append(contentsOf: res) }
+            if let idx = content.index(of: file) {
+                progress.completedUnitCount = Int64(idx + 1)
+            }
+        }
+    }
+}
+
+extension SearchingDuplicatesOperation {
+    override func cancel() {
+        progress.cancel()
+        super.cancel()
+    }
+}
+
+
+// MARK: -  KVO
+
+extension SearchingDuplicatesOperation {
+    private func setProgressObserving() {
+        kvo = progress.observe(\.fractionCompleted) { [unowned self] (progress, change) in
+            print(progress.fractionCompleted)
+            self.delegate?.didUpdateProgress(fractionCompleted: progress.fractionCompleted)
+        }
+    }
+}
