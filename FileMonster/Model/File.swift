7c2de9ee80ff7f81e087718d5d7d0fc76d88dd81
Slava Plis <observleer@gmail.com>
2018-09-02 00:25:04 +0300

    Updated with file attributes.

--- FileMonster/Model/File.swift
+++ FileMonster/Model/File.swift
@@ -7,25 +7,53 @@
 //
 
 import Foundation
+import Cocoa
 
 enum ManagableType {
     case file
     case dir
 }
 
+let attributes = [URLResourceKey.effectiveIconKey,
+                  URLResourceKey.contentModificationDateKey,
+                  URLResourceKey.fileSizeKey,
+                  URLResourceKey.isDirectoryKey]
+
 struct File: Equatable {
-    internal var type: ManagableType
-    internal var path: URL
+    private var type: ManagableType
+    let path: URL
     let name: String
     let ext: String
-
+    
+    var icon: NSImage?
+    var date: Date?
+    var size: Int64?
+    var isFolder: Bool?
+    
     init(with path: URL) {
         self.name = path.lastPathComponent
         self.ext = path.pathExtension
         self.type = .file
         self.path = path
+        
+        guard let props = fetchFileAttributes(path: path) else { return }
+        
+        self.icon = props[URLResourceKey.effectiveIconKey] as? NSImage
+        self.date = props[URLResourceKey.contentModificationDateKey] as? Date
+        self.size = props[URLResourceKey.fileSizeKey] as? Int64
+        self.isFolder = props[URLResourceKey.isDirectoryKey] as? Bool
     }
 
+    private func fetchFileAttributes(path: URL) -> [URLResourceKey : Any]? {
+        do {
+            let props = try (path as NSURL).resourceValues(forKeys: attributes)
+            return props
+        } catch {
+            print("Opps, can't get props for: \(path)")
+            return nil
+        }
+    }
+    
     static func ==(lhs: File, rhs: File) -> Bool {
         return lhs.name == rhs.name && lhs.ext == rhs.ext
     }
